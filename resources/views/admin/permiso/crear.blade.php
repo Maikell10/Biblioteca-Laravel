d5285e4bf3bf7c482c770c2eff9e349fd9d08ba9
Maikell <maikell.ods10@gmail.com>
2019-09-16 12:27:58 -0400

    Permisos

--- resources/views/admin/permiso/crear.blade.php
+++ resources/views/admin/permiso/crear.blade.php
@@ -1,22 +1,34 @@
 @extends("themes.$theme.layout")
-
 @section('titulo')
     Permisos
 @endsection
 
+@section("scripts")
+<script src="{{asset("assets/pages/scripts/admin/permiso/crear.js")}}" type="text/javascript"></script>
+@endsection
+
 @section('contenido')
-    <div class="row">
-        <div class="col-lg-12">
-          <div class="box box-danger">
+<div class="row">
+    <div class="col-lg-12">
+        @include('includes.mensaje')
+        <div class="box box-danger">
             <div class="box-header with-border">
-              <h3 class="box-title">Crear Permisos</h3>
+                <h3 class="box-title">Crear Permisos</h3>
+                <a href="{{route('permiso')}}" class="btn btn-info btn-sm pull-right">Listado</a>
             </div>
-            <!-- /.box-header -->
-            <div class="box-body">
-              <table class="table table-bordered table-hover table-striped">
-                Aqui el formulario
-              </table>
-            </div>
+            <form action="{{route('guardar_permiso')}}" id="form-general" class="form-horizontal" method="POST" autocomplete="off">
+                @csrf
+                <div class="box-body">
+                    @include('admin.permiso.form')
+                </div>
+                <div class="box-footer">
+                    <div class="col-lg-3"></div>
+                    <div class="col-lg-6">
+                        @include('includes.boton-form-crear')
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
+</div>
 @endsection
